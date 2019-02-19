ba30747dc47264bf2988ee5e801bea17924241b3
AzarovEvgenii <esazarov@edu.hse.ru>
2019-02-19 17:07:27 +0300

    Uploade photo functionality was added

--- Shedule.API/Dtos/PhotoForReturnDto.cs
+++ Shedule.API/Dtos/PhotoForReturnDto.cs
@@ -1,16 +1,14 @@
 using System;
 
-namespace Shedule.API.Models
+namespace Shedule.API.Dtos
 {
-    public class Photo
+    public class PhotoForReturnDto
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
         public DateTime TimeAdded { get; set; }
         public bool IsMain { get; set; }
-        public string PublicId {get; set;}
-        public User User { get; set; }
-        public int UserId { get; set; }
+        public string PublicId { get; set; }
     }
 }
--- Shedule.API/Helpers/AutoMapperProfiles.cs
+++ Shedule.API/Helpers/AutoMapperProfiles.cs
@@ -25,6 +25,8 @@ namespace Shedule.API.Helpers
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
